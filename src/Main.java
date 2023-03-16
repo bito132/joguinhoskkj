74b54b39824c8496ff856ad3480b561bb8ac9612
João Vitor Pereira Soareskkj <126785926+bito132@users.noreply.github.com>
2023-03-16 10:54:13 -0300

    jogo bem velhokkkk terminado

--- src/Main.java
+++ src/Main.java
@@ -27,19 +27,19 @@ public class Main {
             for(int t = 0; t < 9; t++) {
 
                 while (true){
-                    System.out.println("Jogaodor A\nInsira uma posição de 1 a 9");
-	                pos_escolhida = sc.nextInt();
-	                
-                	if (blocos_usados.contains(pos_escolhida) == true){
-                		System.out.println("Já escolheram essa ANIMAL!");
-                	}
-                	else {
-                		jogador_a.add(pos_escolhida);
-                		blocos_usados.add(pos_escolhida);
-                		break;
-                	}
+                    System.out.println("Jogaodor A\nInsira uma posiÃ§Ã£o de 1 a 9");
+                    pos_escolhida = sc.nextInt();
+
+                    if ((blocos_usados.contains(pos_escolhida) == true) || (pos_escolhida > 9) || (pos_escolhida < 1)){
+                        System.out.println("Escolhe certo ANIMAL!");
+                    }
+                    else {
+                        jogador_a.add(pos_escolhida);
+                        blocos_usados.add(pos_escolhida);
+                        break;
+                    }
                 }
-                
+
                 mostrar_jogo(jogador_a, jogador_b);
                 if (ganhar(jogador_a, posicoes_para_ganhar) == true) {
                     System.out.println("O jogador A ganhou");
@@ -55,17 +55,17 @@ public class Main {
                 }
 
                 while (true){
-                    System.out.println("Jogaodor B\nInsira uma posição de 1 a 9");
-	                pos_escolhida = sc.nextInt();
-	                
-                	if (blocos_usados.contains(pos_escolhida) == true){
-                		System.out.println("Já escolheram essa ANIMAL!");
-                	}
-                	else {
-                		jogador_b.add(pos_escolhida);
-                		blocos_usados.add(pos_escolhida);
-                		break;
-                	}
+                    System.out.println("Jogaodor B\nInsira uma posiÃ§Ã£o de 1 a 9");
+                    pos_escolhida = sc.nextInt();
+
+                    if ((blocos_usados.contains(pos_escolhida) == true) || (pos_escolhida > 9) || (pos_escolhida < 1)){
+                        System.out.println("Escolhe certo ANIMAL!");
+                    }
+                    else {
+                        jogador_b.add(pos_escolhida);
+                        blocos_usados.add(pos_escolhida);
+                        break;
+                    }
                 }
                 mostrar_jogo(jogador_a, jogador_b);
                 if (ganhar(jogador_b, posicoes_para_ganhar) == true) {
@@ -76,15 +76,15 @@ public class Main {
 
             }
             //if (t == 9)
-              //  System.out.println("\nDeu velha");
+            //  System.out.println("\nDeu velha");
 
         }
 
 
         sc.close();
     }
-    
-    
+
+
     public static boolean ganhar(ArrayList<Integer> posicoes, int[][] posicoes_para_ganhar) {
         int pos_vencer = 0;
 
@@ -93,8 +93,8 @@ public class Main {
                 break;
             pos_vencer = 0;
             for(int p = 0; p < 3; p++) {
-                    if (posicoes.contains(i[p]) == true)
-                        pos_vencer += 1;
+                if (posicoes.contains(i[p]) == true)
+                    pos_vencer += 1;
             }
         }
         if (pos_vencer == 3){
@@ -104,24 +104,24 @@ public class Main {
             return false;
         }
     }
-    
-    
+
+
     public static void mostrar_jogo(ArrayList<Integer> first_jogador, ArrayList<Integer> second_jogador) {
-    	
-    	for (int linha = 0; linha < 3; linha++) {
-    		
-    		for (int quadrante = linha * 3 + 1; quadrante <= linha * 3 + 3; quadrante++) {
-    			if (first_jogador.contains(quadrante) == true) {
-    				System.out.print("|X");
-    			}
-    			else if (second_jogador.contains(quadrante) == true){
-    				System.out.print("|O");
-    			}
-    			else {
-    				System.out.print("| ");
-    			}
-    		}
-    		System.out.println("|");
-    	}
+
+        for (int linha = 0; linha < 3; linha++) {
+
+            for (int quadrante = linha * 3 + 1; quadrante <= linha * 3 + 3; quadrante++) {
+                if (first_jogador.contains(quadrante) == true) {
+                    System.out.print("|X");
+                }
+                else if (second_jogador.contains(quadrante) == true){
+                    System.out.print("|O");
+                }
+                else {
+                    System.out.print("| ");
+                }
+            }
+            System.out.println("|");
+        }
     }
 }
